0319933e8619b5bd91f2eb3159b292b7f59b35bf
Yeonseung Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2023-09-22 17:34:01 +0900

    feat: create login modal

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -1,22 +1,35 @@
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import CameraIcon from '../../svg/CameraIcon';
 import SearchBar from '../common/SearchBar';
+import LoginModal from '../modal/LoginModal';
 
 export default function Header() {
+  const [loginModal, setLoginModal] = useState(false);
   const router = useRouter();
   return (
-    <header className="fixed top-0 z-10 w-screen h-14 md:h-16 px-4 md:px-5 flex justify-between items-center border-b border-zinc-300 bg-white/80 backdrop-blur-sm">
-      <button
-        onClick={() => router.push('/')}
-        className="flex items-center space-x-1"
-      >
-        <CameraIcon className="w-6 h-6 md:w-8 md:h-8" />
-        <h4 className="text-md md:text-2xl font-semibold text-zinc-700">
-          YonnPix
-        </h4>
-      </button>
-      <SearchBar isHeader />
-      <button>sign in</button>
-    </header>
+    <>
+      <LoginModal isOpen={loginModal} closeModal={() => setLoginModal(false)} />
+      <header className="fixed top-0 z-10 w-screen h-14 md:h-16 px-4 md:px-5 flex justify-between items-center border-b border-zinc-300 bg-white/80 backdrop-blur-sm">
+        <button
+          onClick={() => router.push('/')}
+          className="flex items-center space-x-1"
+        >
+          <CameraIcon className="w-5 h-5 md:w-8 md:h-8 max-[380px]:hidden" />
+          <h4 className="text-sm md:text-2xl font-semibold text-zinc-700">
+            YonnPix
+          </h4>
+        </button>
+        <SearchBar isHeader />
+        <button
+          onClick={() => setLoginModal(true)}
+          className="relative inline-flex items-center justify-center p-0.5 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 hover:text-white focus:ring-0"
+        >
+          <span className="relative px-3 py-1 md:px-4 md:py-2 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
+            Log in
+          </span>
+        </button>
+      </header>
+    </>
   );
 }
