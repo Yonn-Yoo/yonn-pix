3ed86d64df0b1adb68b073400ee703d466c4cfb6
Yonn-Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2024-07-19 14:09:19 +0900

    refactor: refactor api functions by removing status check

--- src/api/service.ts
+++ src/api/service.ts
@@ -4,34 +4,22 @@ import initAxios from './api';
 const axios = initAxios();
 
 export const getRandomPhotos = async (count: number) => {
-  const response = await axios.get('/photos/random', {
+  const { data: images } = await axios.get('/photos/random', {
     params: {
       count,
     },
   });
 
-  if (response.status !== 200) {
-    throw new Error('Sorry, something went wrong 😢');
-  }
-
-  const images = response.data;
-
   return images;
 };
 
 export const searchPhotos = async (reqBody: searchReqBodyType) => {
-  const response = await axios.get('/search/photos', {
+  const { data: images } = await axios.get('/search/photos', {
     params: {
       ...reqBody,
       per_page: 30,
     },
   });
-
-  if (response.status !== 200) {
-    throw new Error('Sorry, something went wrong 😢');
-  }
-
-  const images = response.data;
 
   return images;
 };
