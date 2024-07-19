c552c4e8474a2f6e29290de73838b8eabb6a2904
Yonn-Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2024-07-19 10:30:25 +0900

    style: adjust height of detailmodal for mobile

--- src/components/modal/DetailModal.tsx
+++ src/components/modal/DetailModal.tsx
@@ -78,28 +78,28 @@ export default function DetailModal({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 md:scale-90 max-md:translate-y-72"
             >
-              <Dialog.Panel className="flex flex-col space-y-4 w-screen md:w-full max-w-screen-md lg:max-w-screen-lg transform rounded-xl max-md:rounded-b-none bg-white backdrop-blur p-6 text-left shadow-xl">
-                <div className="w-full flex items-center justify-between">
-                  <div className="flex items-center space-x-3">
+              <Dialog.Panel className="flex flex-col max-md:h-[76vh] overflow-y-auto space-y-4 w-screen md:w-full md:max-w-screen-md lg:max-w-screen-lg transform rounded-xl max-md:rounded-b-none bg-white backdrop-blur p-6 text-left shadow-xl">
+                <div className="w-full flex max-md:flex-col max-md:space-y-2 items-end md:items-center justify-between">
+                  <div className="w-full flex items-center space-x-3">
                     <div
-                      className="w-9 h-9 rounded-full"
+                      className="w-8 h-8 md:w-9 md:h-9 rounded-full"
                       style={{
                         backgroundImage: `url(${user?.profile_image.small})`,
                       }}
                     />
-                    <div className="flex flex-col -space-y-1">
-                      <h2 className="text-lg md:text-xl capitalize font-semibold text-zinc-900">
+                    <div className="flex flex-col md:-space-y-1">
+                      <h2 className="md:text-lg lg:text-xl capitalize font-semibold text-zinc-900">
                         {user?.first_name} {user?.last_name}{' '}
-                        <strong className="font-normal text-zinc-500 text-sm md:text-base">
+                        <strong className="font-normal text-zinc-500 text-xs md:text-sm lg:text-base">
                           on Unsplash
                         </strong>
                       </h2>
-                      <span className="text-sm text-slate-500">
+                      <span className="text-xs md:text-sm text-slate-500">
                         {user?.username}
                       </span>
                     </div>
                   </div>
-                  <div className="flex items-center bg-[#3DB46E] hover:bg-opacity-80 rounded-lg">
+                  <div className="w-fit flex items-center bg-[#3DB46E] hover:bg-opacity-80 rounded-lg">
                     <button
                       onClick={() => handleDownload()}
                       className="text-sm text-white pl-4 pr-3 py-2"
@@ -116,35 +116,41 @@ export default function DetailModal({
                   </div>
                 </div>
                 <img
-                  className="h-[50vh] object-contain"
+                  className="h-2/3 md:h-[50vh] object-contain"
                   src={urls.regular}
                   alt={alt}
                 />
                 <section>
                   <div className="flex space-x-10">
                     <div className="flex flex-col">
-                      <span className="lg:text-lg font-medium">Views</span>
-                      <span className="text-slate-600">
+                      <span className="text-sm md:text-base lg:text-lg font-medium">
+                        Views
+                      </span>
+                      <span className="max-md:text-sm text-slate-600">
                         {views?.toLocaleString() || 0}
                       </span>
                     </div>
                     <div className="flex flex-col">
-                      <span className="lg:text-lg font-medium">Likes</span>
-                      <span className="text-slate-600">
+                      <span className="text-sm md:text-base lg:text-lg font-medium">
+                        Likes
+                      </span>
+                      <span className="max-md:text-sm text-slate-600">
                         {likes?.toLocaleString() || 0}
                       </span>
                     </div>
                     <div className="flex flex-col">
-                      <span className="lg:text-lg font-medium">Downloads</span>
-                      <span className="text-slate-600">
+                      <span className="text-sm md:text-base lg:text-lg">
+                        Downloads
+                      </span>
+                      <span className="max-md:text-sm text-slate-600">
                         {downloads?.toLocaleString() || 0}
                       </span>
                     </div>
                   </div>
-                  <h2 className="mt-5 text-lg lg:text-xl font-semibold">
+                  <h2 className="mt-3 md:mt-5 md:text-lg lg:text-xl font-semibold">
                     {alt}
                   </h2>
-                  <time className="text-slate-500">
+                  <time className="max-md:text-sm text-slate-500">
                     Posted at {moment(data.updated_at).format('YYYY. MM. DD')}
                   </time>
                 </section>
